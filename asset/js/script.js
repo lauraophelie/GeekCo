1c6406639b2e5bdb86b899462a723a020f0f2b42
NyKanto-crypto <nykantorandri@gmail.com>
2023-07-05 19:47:14 +0300

    ny kanto

---
+++ asset/js/script.js
@@ -0,0 +1,65 @@
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("open");
+  menuBtnChange();//calling the function(optional)
+});
+
+searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+  sidebar.classList.toggle("open");
+  menuBtnChange(); //calling the function(optional)
+});
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+ if(sidebar.classList.contains("open")){
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+ }else {
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+ }
+}
+
+//insert
+const multiStepForm = document.querySelector("[data-multi-step]")
+const formSteps = [...multiStepForm.querySelectorAll("[data-step]")]
+let currentStep = formSteps.findIndex(step => {
+  return step.classList.contains("active")
+})
+
+if (currentStep < 0) {
+  currentStep = 0
+  showCurrentStep()
+}
+
+multiStepForm.addEventListener("click", e => {
+  let incrementor
+  if (e.target.matches("[data-next]")) {
+    incrementor = 1
+  } else if (e.target.matches("[data-previous]")) {
+    incrementor = -1
+  }
+
+  if (incrementor == null) return
+
+  const inputs = [...formSteps[currentStep].querySelectorAll("input")]
+  const allValid = inputs.every(input => input.reportValidity())
+  if (allValid) {
+    currentStep += incrementor
+    showCurrentStep()
+  }
+})
+
+formSteps.forEach(step => {
+  step.addEventListener("animationend", e => {
+    formSteps[currentStep].classList.remove("hide")
+    e.target.classList.toggle("hide", !e.target.classList.contains("active"))
+  })
+})
+
+function showCurrentStep() {
+  formSteps.forEach((step, index) => {
+    step.classList.toggle("active", index === currentStep)
+  })
+}
